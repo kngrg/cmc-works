08204aad58f20f1a3add9790637720191d633282
kngrg <kaengreg654@gmail.com>
2022-04-20 17:26:59 +0300

    Create 7.asm

---
+++ asm-codes/hw/hw-04.20/7.asm
@@ -0,0 +1,10 @@
+a)
+    D1 DATE <1945, 5, 13, 'среда'>
+    D2 DATE <,12,,'четверг'>
+    D3 DATE <1980>
+    D4 DATE <>
+b)
+    1) type = size D1 = size DATE = 15
+    2) type D1.Y = 2, type D1.D = 1, type D1.WD = 1
+    3) length D1.Y = 1, length D1.WD = 1, size D1.Y = 2, size D1.WD = 1
+    4) 103h
